9b65bfd5a1a7cc6b25c3ad39a839934f2795f087
Ian T Price <Ian.T.Price@centreline.biz>
2024-11-11 18:55:20 +0000

    correcting google code...

--- web/src/components/furniture/header.tsx
+++ web/src/components/furniture/header.tsx
@@ -8,7 +8,7 @@ export default component$(() => {
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-D2MW5NBL5Z"></script>
       <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+        function gtag(){dataLayer.push(arguments)};
         gtag('js', new Date());
 
         gtag('config', 'G-D2MW5NBL5Z');
